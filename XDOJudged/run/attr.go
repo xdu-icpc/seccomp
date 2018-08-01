cd04ada43233623c27a175e462454a3bc0486fe3
Xi Ruoyao <ryxi@stu.xidian.edu.cn>
2018-08-01 14:24:08 +0800

    [run] Add comment about CPU time limit and seccomp filter

--- XDOJudged/run/attr.go
+++ XDOJudged/run/attr.go
@@ -7,7 +7,10 @@ import (
 // Attr holds the attributes that will be applied to a new process started
 // by this package.
 type Attr struct {
+	// If this field is not nil, a seccomp filter will be used to prevent
+	// forking since we have no way to limit a process group's CPU time.
 	CPUTimeLimit  *time.Duration
+
 	WallTimeLimit *time.Duration
 	// TODO: Fields for cgroups...
 }
