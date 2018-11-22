01b1213390179d711b486acf47d065d427d4bba1
Xi Ruoyao <xry111@mengyan1223.wang>
2018-11-23 04:50:13 +0800

    Add copyright disclaimer and file description

--- XDOJudged/cgroup/cgroup.go
+++ XDOJudged/cgroup/cgroup.go
@@ -1,5 +1,25 @@
+// Cgroup basic functions
+// Copyright (C) 2018  Laboratory of ICPC, Xidian University
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Author: Xi Ruoyao <xry111@mengyan1223.wang>
+
 // +build linux
 
+// package cgroup provides native Go interface to manipulate Linux
+// cgroups(7) for resource control.
 package cgroup
 
 import (
--- XDOJudged/cgroup/controller.go
+++ XDOJudged/cgroup/controller.go
@@ -1,3 +1,21 @@
+// Cgroup controller definition and pretty printer.
+// Copyright (C) 2018  Laboratory of ICPC, Xidian University
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Author: Xi Ruoyao <xry111@mengyan1223.wang>
+
 package cgroup
 
 type Controller int
